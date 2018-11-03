11c33c3eaed9d351b6fe1ae22d110328f92ecac5
Isaac Vandor <isaacvandor@gmail.com>
2018-11-03 16:38:32 -0400

    Removed dataset

--- data_processing_utilities/scripts/wav_process.py
+++ data_processing_utilities/scripts/wav_process.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
 
 from pydub import AudioSegment
+import soundfile as sf
+import time
 
-t1 = 3000 #Works in milliseconds
-t2 = 4500
-newAudio = AudioSegment.from_wav("/home/nina/yes.wav")
-newAudio = newAudio[t1:t2]
-newAudio.export('/home/nina/catkin_ws/src/audio_bot/robot_learning/data_processing_utilities/data/yes.wav', format="wav") #Exports to a wav file in the current path.
+#'/home/nina/catkin_ws/src/audio_bot/robot_learning/data_processing_utilities
+size_threshold = 23000
+T1 = 0
+T2 = 1500 #milliseconds
+Interval = 100
+stream_file = '../data/stream_recordings/stream.wav'
+newAudio = AudioSegment.from_wav(stream_file)
+time.sleep(1.5)
+print("Wav files starting to be written")
+i = 0
+
+while sf.info(stream_file).duration > (T2/1000):
+    segment = newAudio[T1:T2]
+    name = str(i)
+    segment.export('../data/ogg_files/%s.ogg' %name, format="ogg")
+    size = sf.info('../data/ogg_files/%s.ogg' %name, verbose=True).extra_info
+    beg = size.index("Length")+9
+    end = size.index("\n", beg)
+    size = int(size[beg:end])
+    if size > size_threshold:
+        segment.export('../data/stream_recordings/%s.wav' %name, format="wav")
+    i += 1
+    T1 += Interval
+    T2 += Interval
+    time.sleep(Interval/1000)
